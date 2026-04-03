ebfa7de776544b8b636f15f84d3c6046e2b76548
Luthando17 <Luthando00@icloud.com>
2026-04-04 01:50:00 +0200

    Day 03 : Solved Valid Anagram using freq counting of strings with incre & decre.

--- Day 03/Valid_Anagram.java
+++ Day 03/Valid_Anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if(s.length() != t.length()){ // auto false if num of char does'nt compare.
           return false;
         }
 
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++){
-          count[s.charAt(i) - 'a'] ++ ;
-          count[t.charAt(i) - 'a'] -- ;
+          count[s.charAt(i) - 'a'] ++ ; //increament because of you including the count of char.
+          count[t.charAt(i) - 'a'] -- ; //decrease to compare the count of char matches by subing each you come across. 
         }
         for(int value : count){
           if(value != 0){
@@ -17,3 +17,17 @@ class Solution {
         return true;
     }
 }
+
+/*  ----------REMEMBER-------------
+
+
+  VALID ANAGRAM
+  This is typically a String that contains the exact same char as another
+  string, but order of char doesnt matter
+
+  eg s = "car" , t = "rat"
+    = false (Strings dont match)
+
+    s = "racecar" , t = "carrace"
+    = true (both strings contain duplicated char)
+*/
