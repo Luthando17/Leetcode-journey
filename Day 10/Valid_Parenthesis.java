71c2931236b248b62b7f5a1808790394959eda87
Luthando17 <Luthando00@icloud.com>
2026-04-18 00:08:40 +0200

    Day 10: Solved Valid Parenthesis using Stack.

---
+++ Day 10/Valid_Parenthesis.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> openStack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char bracket = s.charAt(i);
+            if(bracket == '(' || bracket == '{' || bracket == '['){
+                openStack.push(bracket);
+            } else if (openStack.isEmpty()) {
+                return false;
+            } else if(bracket == ')' && openStack.peek()== '('){
+                openStack.pop();
+            } else if(bracket == '}' && openStack.peek()== '{'){
+                openStack.pop();
+            } else if(bracket == ']' && openStack.peek()== '['){
+                openStack.pop();
+            } else {
+                return false;
+            }
+        }
+        if(openStack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
+
+
+/*
+REMEMBER!!!
+
+This problem uses  STACK case ,the ability to keep track 
+of the most inserted recent element.
+The STACK uses a formula LIFO (last, in, first, out)
+
+ and STACK works as a temp storage of given elements to keep
+ track
+
+ * .push();
+   .peek();
+   .pop();
+
+ */
